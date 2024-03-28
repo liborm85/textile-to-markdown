79b589b7cf5bd99641a09316149215590a72abae
Libor M <liborm85@gmail.com>
2024-03-28 17:45:45 +0100

    pandoc transformation

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Textile to Markdown convertor
 
+## Requirements
+
+- [Pandoc](https://pandoc.org/) installed from [GitHub](https://github.com/jgm/pandoc/releases/latest)
+
+## Usage
+
+- Copy textile files to `input` folder.
+- Run command `sh convert.sh`
+- The converted files to markdown are in `output` folder.
+
+## License
+
+MIT
---
+++ convert.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# $1 - regex
+# $2 - pagename
+# $3 - extension
+function replace_code() {
+  local lastStepNumber=$stepsCounter
+  stepsCounter=$((stepsCounter+1))
+  sed -e "$1" \
+    tmp/$2-$lastStepNumber.$3 > tmp/$2-$stepsCounter.$3
+}
+
+# $1 - regex
+# $2 - pagename
+# $3 - extension
+function replace_code_regex() {
+  local lastStepNumber=$stepsCounter
+  stepsCounter=$((stepsCounter+1))
+  sed -r -e "$1" \
+    tmp/$2-$lastStepNumber.$3 > tmp/$2-$stepsCounter.$3
+}
+
+# cleanup folders
+rm tmp/*.textile
+rm tmp/*.md
+rm output/*.md
+
+# convert all textile files
+for filename in input/*.textile
+do
+  pagename="$(basename -- $filename .textile)"
+  echo "$filename (PAGE: $pagename)"
+
+  stepsCounter="0"
+
+  cp $filename tmp/$pagename-0.textile
+
+  lastStepNumber=$stepsCounter
+  stepsCounter=$((stepsCounter+1))
+
+  # transformation from textile to markdown using Pandoc
+  pandoc tmp/$pagename-$lastStepNumber.textile -o tmp/$pagename-$stepsCounter.md --from textile --to gfm --wrap=none
+
+  # final markdown output
+  cp tmp/$pagename-$stepsCounter.md output/$pagename.md
+done
